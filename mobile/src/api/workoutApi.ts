05a7ef4033a1f901b683d5e046bfb82605f0d008
Lukasz Wojcik <arbor.software@gmail.com>
2026-09-02 14:32:32 +0200

    das

--- mobile/src/api/workoutApi.ts
+++
@@ -1,7 +0,0 @@
-import { apiClient } from './apiClient'
-import type { CompleteRoutineCreate, WorkoutHistoryOut } from '../types'
-
-export async function completeRoutine(payload: CompleteRoutineCreate): Promise<WorkoutHistoryOut> {
-  const { data } = await apiClient.post<WorkoutHistoryOut>('/api/routines/complete', payload)
-  return data
-}
